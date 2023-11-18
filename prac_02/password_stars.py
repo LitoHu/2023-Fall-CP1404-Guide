3c16f67dfcbecb2cef0725ea2b3b23c9da3205e0
LitoHu <shiqi.hu@my.jcu.edu.au>
2023-11-18 14:53:38 +0800

    Add password check program

---
+++ prac_02/password_stars.py
@@ -0,0 +1,12 @@
+
+minimum_length = 5
+    # Ask the user for a password
+    password = input("Please enter a password: ")
+
+    # Check if the password meets the minimum length requirement
+    if len(password) < minimum_length:
+        print(f"Password must be at least {minimum_length} characters long. Try again.")
+    else:
+        # Print asterisks based on the password length
+        print("*" * len(password))
+
